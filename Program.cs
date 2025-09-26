b45d60914fffbf70b8dcc22cb885e8aef1cc322e
Rendszergazda <rendszergazda@blathy.info>
2025-09-26 14:08:42 +0200

    Szinezo

--- Program.cs
+++ Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.SetCursorPosition(Console.WindowWidth / 2, Console.WindowHeight / 2);
+            while (true)
+            {
+                var key = Console.ReadKey().Key;
+                switch (key)
+                {
+                    case ConsoleKey.DownArrow:
+                        Console.CursorTop++;
+                        Console.CursorLeft--;
+                        Console.Write("*");
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Console.CursorTop--;
+                        Console.CursorLeft--;
+                        Console.Write("*");
+                        break;
+                    case ConsoleKey.RightArrow:
+                        Console.Write("*");
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.CursorLeft = Console.CursorLeft - 2;
+                        Console.Write("*");
+                        break;
+                }
+            }
         }
     }
 }
